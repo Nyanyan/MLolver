bc49085f640fd1ad68b7599e3601a4f41e752dec
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-08-15 09:49:03 +0900

    idx

--- cube_class.py
+++ cube_class.py
@@ -92,149 +92,31 @@ class Cube:
     def move(self, mov):
         return Cube(cp=self.move_cp(mov), co=self.move_co(mov), ep=self.move_ep(mov), eo=self.move_eo(mov))
     
-    def idx_cp(self):
-        res = 0
+    def idx(self):
+        idx_cp = 0 # max 40320
         for i in range(8):
             cnt = self.Cp[i]
             for j in self.Cp[:i]:
                 if j < self.Cp[i]:
                     cnt -= 1
-            res += fac[7 - i] * cnt
-        return res
-    
-    def idx_co(self):
-        res = 0
+            idx_cp += fac[7 - i] * cnt
+        idx_co = 0 # max 2187
         for i in range(7):
-            res *= 3
-            res += self.Co[i]
-        return res
-
-    def idx_ep(self):
-        mse_parts = [[2, 0, 10, 8], [3, 1, 9, 11], [4, 5, 6, 7]]
-        arr = [[-1 for _ in range(4)] for _ in range(3)]
+            idx_co *= 3
+            idx_co += self.Co[i]
+        idx_ep = 0 # max 479001600
         for i in range(12):
-            for mse in range(3):
-                if self.Ep[i] in mse_parts[mse]:
-                    arr[mse][mse_parts[mse].index(self.Ep[i])] = i
-        #print(arr)
-        res = [-1 for _ in range(3)]
-        for mse in range(3):
-            tmp = 0
-            for i in range(4):
-                cnt = arr[mse][i]
-                for j in arr[mse][:i]:
-                    if j < arr[mse][i]:
-                        cnt -= 1
-                tmp += cnt * cmb(11 - i, 3 - i) * fac[3 - i]
-            res[mse] = tmp
-        return res
-    
-    def idx_eo(self):
-        res_eo = 0
+            cnt = self.Ep[i]
+            for j in self.Ep[:i]:
+                if j < self.Ep[i]:
+                    cnt -= 1
+            idx_ep += fac[12 - i] * cnt
+        idx_eo = 0 # max 2048
         for i in range(11):
-            res_eo *= 2
-            res_eo += self.Eo[i]
-        return res_eo
-    
-    def solved_ep_phase0(self):
-        for i in range(4, 8):
-            if not self.Ep[i] in {4, 5, 6, 7}:
-                return False
-        return True
+            idx_eo *= 2
+            idx_eo += self.Eo[i]
+        return idx_cp, idx_co, idx_ep, idx_eo
 
-    '''
-    def idx_phase0_ep(self):
-        res_ep = 0
-        cnt = 0
-        for i in range(12):
-            if cnt == 4:
-                break
-            if self.Ep[i] // 4 == 1:
-                res_ep += cmb(11 - i, 4 - cnt)
-                cnt += 1
-        return res_ep
-    
-    def idx_phase0_eo(self):
-        res_eo = 0
-        for i in range(11):
-            res_eo *= 2
-            res_eo += self.Eo[i]
-        return res_eo
-    
-    def idx_phase1_ep_ud(self):
-        res_ep_ud = 0
-        arr_ep_ud = [self.Ep[i] for i in [0, 1, 2, 3, 8, 9, 10, 11]]
-        for i in range(8):
-            if arr_ep_ud[i] > 3:
-                arr_ep_ud[i] -= 4
-        for i in range(8):
-            cnt = arr_ep_ud[i]
-            for j in arr_ep_ud[:i]:
-                if j < arr_ep_ud[i]:
-                    cnt -= 1
-            res_ep_ud += fac[7 - i] * cnt
-        return res_ep_ud
-
-    def idx_phase1_ep_fbrl(self):
-        res_ep_fbrl = 0
-        arr_ep_fbrl = [self.Ep[i] - 4 for i in [4, 5, 6, 7]]
-        for i in range(4):
-            cnt = arr_ep_fbrl[i]
-            for j in arr_ep_fbrl[:i]:
-                if j < arr_ep_fbrl[i]:
-                    cnt -= 1
-            res_ep_fbrl += fac[3 - i] * cnt
-        return res_ep_fbrl
-    '''
-    '''
-    def idx_phase0(self):
-        res_co = 0
-        for i in range(7):
-            res_co *= 3
-            res_co += self.Co[i]
-        res_ep = 0
-        cnt = 0
-        for i in range(12):
-            if cnt == 4:
-                break
-            if self.Ep[i] // 4 == 1:
-                res_ep += cmb(11 - i, 4 - cnt)
-                cnt += 1
-        res_eo = 0
-        for i in range(11):
-            res_eo *= 2
-            res_eo += self.Eo[i]
-        return res_co, res_ep + res_eo * 495
-    
-    def idx_phase1(self):
-        res_cp = 0
-        for i in range(8):
-            cnt = self.Cp[i]
-            for j in self.Cp[:i]:
-                if j < self.Cp[i]:
-                    cnt -= 1
-            res_cp += fac[7 - i] * cnt
-        res_ep_ud = 0
-        arr_ep_ud = [self.Ep[i] for i in [0, 1, 2, 3, 8, 9, 10, 11]]
-        for i in range(8):
-            if arr_ep_ud[i] > 3:
-                arr_ep_ud[i] -= 4
-        for i in range(8):
-            cnt = arr_ep_ud[i]
-            for j in arr_ep_ud[:i]:
-                if j < arr_ep_ud[i]:
-                    cnt -= 1
-            res_ep_ud += fac[7 - i] * cnt
-        res_ep_fbrl = 0
-        arr_ep_fbrl = [self.Ep[i] - 4 for i in [4, 5, 6, 7]]
-        for i in range(4):
-            cnt = arr_ep_fbrl[i]
-            for j in arr_ep_fbrl[:i]:
-                if j < arr_ep_fbrl[i]:
-                    cnt -= 1
-            res_ep_fbrl += fac[3 - i] * cnt
-        return res_cp, res_ep_ud * 24 + res_ep_fbrl
-    '''
 def face(twist):
     return twist // 3
 
